07aa968751e1eac46f2f605b15ecab0dc5a9a5e8
Irwin Gonzales <irwingonzales@ymail.com>
2015-10-23 14:56:44 -0700

    Initial Commit

---
+++ ProjectIV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectIV
+//
+//  Created by Irwin Gonzales on 10/23/15.
+//  Copyright (c) 2015 Irwin Gonzales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectIV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectIV
+//
+//  Created by Irwin Gonzales on 10/23/15.
+//  Copyright (c) 2015 Irwin Gonzales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
